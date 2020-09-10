789c6970105fd73edbecb024ac20669428e12b2e
Yong <zhuyongdan@zhongmei66.com>
2020-09-10 16:19:35 +0800

    fixed:命名空间

--- test/TestLock.php
+++ test/TestLock.php
@@ -2,13 +2,12 @@
 // 测试脚本，建议并发开启多个脚本来看效果
 
 require_once '../vendor/autoload.php';
-// require_once '../src/LocalLock.php';
 
-use Lostmilky\LocalLock;
+use Lostmilky\LocalLock\LocalLock;
 
 $mark = mt_rand(1000, 9999);
 $key = 'a';
-for ($i=0; $i < 10; $i++) { 
+for ($i=0; $i < 10; $i++) {
     LocalLock::lock($key);
     echo $mark, ':', time(), PHP_EOL;
     $sec = mt_rand(1, 6);
